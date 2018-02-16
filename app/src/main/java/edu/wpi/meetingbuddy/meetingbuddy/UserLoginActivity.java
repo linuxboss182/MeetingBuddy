46420f04b44d371c4b6f17f66923ebe89b596e4d
Anh D Ha <haan@onid.oregonstate.edu>
2018-02-15 23:49:53 -0500

    Added UserLoginActivity + layout

---
+++ app/src/main/java/edu/wpi/meetingbuddy/meetingbuddy/UserLoginActivity.java
@@ -0,0 +1,78 @@
+package edu.wpi.meetingbuddy.meetingbuddy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+/**
+ * Created by Anh on 2/15/2018.
+ */
+
+public class UserLoginActivity extends Activity {
+
+
+    private EditText username;
+    private EditText password;
+    private Button login;
+    private Button create_account;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_log_in);
+
+        username = findViewById(R.id.username);
+        username.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String user_name = s.toString();
+                //Save the username for authentication
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        password = findViewById(R.id.password);
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String user_password = s.toString();
+                //Save the password for authentication
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        login = findViewById(R.id.log_in);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log in
+            }
+        });
+
+
+    }
+}

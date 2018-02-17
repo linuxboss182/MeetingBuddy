f28b07b8614108bd00b1a1cac582f05e839cfffa
Steven McAteer <smcateer@wpi.edu>
2018-02-17 12:06:56 -0500

    Added time picker and datepickers

--- app/src/main/java/edu/wpi/meetingbuddy/meetingbuddy/CreateMeetingActivity.java
+++ app/src/main/java/edu/wpi/meetingbuddy/meetingbuddy/CreateMeetingActivity.java
@@ -2,13 +2,16 @@ package edu.wpi.meetingbuddy.meetingbuddy;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.format.Time;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -20,9 +23,12 @@ import java.util.Locale;
 
 public class CreateMeetingActivity extends Activity {
 
-    EditText dateEditText;
+    EditText dateET;
+    EditText timeET;
     DatePickerDialog.OnDateSetListener date;
+    TimePickerDialog.OnTimeSetListener time;
     Calendar myCalendar;
+    Time myTime;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,7 +36,8 @@ public class CreateMeetingActivity extends Activity {
         setContentView(R.layout.create_meeting);
 
         myCalendar = Calendar.getInstance();
-        dateEditText = findViewById(R.id.dateET);
+        dateET = findViewById(R.id.dateET);
+        timeET = findViewById(R.id.timeET);
 
 
         ////////// Listeners ///////////////////////////
@@ -42,12 +49,11 @@ public class CreateMeetingActivity extends Activity {
                 myCalendar.set(Calendar.YEAR, year);
                 myCalendar.set(Calendar.MONTH, monthOfYear);
                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateLabel();
+                updateDate();
             }
-
         };
 
-        dateEditText.setOnClickListener(new View.OnClickListener() {
+        dateET.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -56,14 +62,47 @@ public class CreateMeetingActivity extends Activity {
                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+
+        time = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                if (view.isShown()) {
+                    myCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                    myCalendar.set(Calendar.MINUTE, minute);
+                    updateTime();
+                }
+            }
+        };
+
+        timeET.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                int hour = myCalendar.get(Calendar.HOUR_OF_DAY);
+                int minute = myCalendar.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog = new TimePickerDialog(CreateMeetingActivity.this, time, hour, minute, false);
+                timePickerDialog.setTitle("Choose time:");
+                timePickerDialog.show();
+            }
+        });
+
+
+
     }
 
 
-    private void updateLabel() {
+    private void updateDate() {
         String myFormat = "MM/dd/yy"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
-        dateEditText.setText(sdf.format(myCalendar.getTime()));
+        dateET.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    private void updateTime() {
+        String myFormat = "HH:MM"; //In which you need put here
+        //TimeFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        //dateET.setText(sdf.format(myCalendar.getTime()));
     }
 
 }

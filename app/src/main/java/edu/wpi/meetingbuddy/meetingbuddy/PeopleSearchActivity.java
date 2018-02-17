30d0474a22d0da69f45e222f05be0aa96f3c519f
Steven McAteer <smcateer@wpi.edu>
2018-02-17 14:19:04 -0500

    Added sexy material search bar ughhhh  yeah

--- app/src/main/java/edu/wpi/meetingbuddy/meetingbuddy/PeopleSearchActivity.java
+++ app/src/main/java/edu/wpi/meetingbuddy/meetingbuddy/PeopleSearchActivity.java
@@ -5,16 +5,53 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
 public class PeopleSearchActivity extends AppCompatActivity {
+
+    MaterialSearchView materialSearchView;
+    String[] list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_people_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        list = new String[]{"Stove", "Paul", "Jesse"};
+
+        materialSearchView = findViewById(R.id.searchBar);
+        materialSearchView.closeSearch();
+        materialSearchView.setSuggestions(list);
+        materialSearchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //Filtering
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Make change realtime
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        MenuItem item = menu.findItem(R.id.search);
+        materialSearchView.setMenuItem(item);
+        return true;
+
     }
 
 }

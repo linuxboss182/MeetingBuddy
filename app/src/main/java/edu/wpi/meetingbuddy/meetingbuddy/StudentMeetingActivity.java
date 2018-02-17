5f4508dd3f616b7abce031cd5d1613e7411fdb1d
Paul <pshingletoniv@gmail.com>
2018-02-17 12:47:50 -0500

    actually added student view

--- app/src/main/java/edu/wpi/meetingbuddy/meetingbuddy/StudentMeetingActivity.java
+++ app/src/main/java/edu/wpi/meetingbuddy/meetingbuddy/StudentMeetingActivity.java
@@ -1,8 +1,16 @@
 package edu.wpi.meetingbuddy.meetingbuddy;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Paul on 2/17/2018.
  */
 
-public class StudentMeetingActivity {
+public class StudentMeetingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_meeting_view);
+    }
 }

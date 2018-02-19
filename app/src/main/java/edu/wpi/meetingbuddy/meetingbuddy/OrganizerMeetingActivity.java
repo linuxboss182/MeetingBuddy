ab41105fc6693ce5a349f28e86969413dc63e5d6
Paul <pshingletoniv@gmail.com>
2018-02-19 13:48:48 -0500

    Finished student, organizer views

---
+++ app/src/main/java/edu/wpi/meetingbuddy/meetingbuddy/OrganizerMeetingActivity.java
@@ -0,0 +1,16 @@
+package edu.wpi.meetingbuddy.meetingbuddy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Paul on 2/19/2018.
+ */
+
+public class OrganizerMeetingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.organizer_meeting_view);
+    }
+}

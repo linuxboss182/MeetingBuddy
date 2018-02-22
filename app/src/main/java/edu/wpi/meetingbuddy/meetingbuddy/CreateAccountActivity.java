066314ae56c0280b2df68b38ca3502d8c2dc7c38
Anh D Ha <haan@onid.oregonstate.edu>
2018-02-21 23:28:01 -0500

    Tried Creating new account

--- app/src/main/java/edu/wpi/meetingbuddy/meetingbuddy/CreateAccountActivity.java
+++ app/src/main/java/edu/wpi/meetingbuddy/meetingbuddy/CreateAccountActivity.java
@@ -9,6 +9,16 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 /**
  * Created by Anh on 2/17/2018.
@@ -20,7 +30,15 @@ public class CreateAccountActivity extends AppCompatActivity {
     private EditText password;
     private EditText first;
     private EditText last;
+    private EditText phonenum;
     private Button create;
+    String user_name;
+    String user_password;
+    String firstname;
+    String lastname;
+    String phone;
+    String schedule = "";
+    private NetworkManager networkManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,7 +55,7 @@ public class CreateAccountActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                String user_name = s.toString();
+                user_name = s.toString();
                 //Save the username for authentication
             }
 
@@ -57,7 +75,7 @@ public class CreateAccountActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                String user_password = s.toString();
+                user_password = s.toString();
                 //Save the password for authentication
             }
 
@@ -75,7 +93,7 @@ public class CreateAccountActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                String first = s.toString();
+                firstname = s.toString();
                 //Save first name
             }
             @Override
@@ -90,8 +108,25 @@ public class CreateAccountActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                String last = s.toString();
+                lastname = s.toString();
                 //Save last name
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        //phonenum
+        phonenum = findViewById(R.id.phonenum);
+        phonenum.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                phone = s.toString();
+                //Save phone
             }
             @Override
             public void afterTextChanged(Editable s) {
@@ -105,14 +140,73 @@ public class CreateAccountActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //Create account
                 //...
+                JSONObject creds = new JSONObject();
+                try {
+                    creds.put("username", user_name);
+                    creds.put("password", user_password);
+                    creds.put("phone", phone);
+                    creds.put("first_name", firstname);
+                    creds.put("last_name", lastname);
+                    creds.put("schedule", schedule);
 
-                //Open a new blank student meetings view
-                Intent i = new Intent(getApplicationContext(), StudentMeetingActivity.class);
-                startActivity(i);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                networkManager.post(NetworkManager.url+"/newAccount", creds.toString(), new Callback() {
+                    @Override
+                    public void onFailure(Request request, IOException e) {
+                        System.out.println("Failed to connect");
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getApplicationContext(), "Failed to connect", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                    @Override
+                    public void onResponse(Response response) throws IOException {
+                        final String responseStr = response.body().string();
+                        final int statusCode = response.code();
+                        try {
+                            final JSONObject jsonRes = new JSONObject(responseStr);
+                            final String status = jsonRes.getString("status");
+
+//                        final String messageText = "Status code : " + response.code() +
+//                                "n" +
+//                                "Response body : " + responseStr;
+//                        System.out.println("Received response");
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getApplicationContext(), status, Toast.LENGTH_LONG).show();
+                                }
+                            });
+
+
+                            if(status.equals("success")) {
+                                //Get my account information
+                                Account myAccount = new Account();
+                                myAccount.fromJSON(jsonRes);
+
+                                //Go to My meetings, send it my account information
+                                Intent i = new Intent(getApplicationContext(), MyMeetingActivity.class);
+                                i.putExtra("Account", myAccount);
+                                startActivity(i);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
             }
         });
 
+                //Open a new blank student meetings view
+                //Intent i = new Intent(getApplicationContext(), UserLoginActivity.class);
+                //startActivity(i);
+            }
 
-    }
 
 }

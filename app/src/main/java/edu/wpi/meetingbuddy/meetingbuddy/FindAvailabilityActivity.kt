3b5400cc100f58410053cdc69f04c07421182fb4
jtfakult <jtfakult@wpi.edu>
2018-02-24 18:19:50 -0500

    Finally finally dear lord save me got the when2meet to work, uploaded as an external library, "When3Meet"

---
+++ app/src/main/java/edu/wpi/meetingbuddy/meetingbuddy/FindAvailabilityActivity.kt
@@ -0,0 +1,16 @@
+package edu.wpi.meetingbuddy.meetingbuddy
+
+import android.graphics.Color
+import android.graphics.RectF
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class FindAvailabilityActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_availability)
+
+
+    }
+}

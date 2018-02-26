0015e459711390c3e7c1fd40a8fe1a804f178309
Jesse Gaulin <jtgaulin@wpi.edu>
2018-02-25 22:10:04 -0500

    added filter on get meeting attendance

--- server/API/API.js
+++ server/API/API.js
@@ -215,7 +215,7 @@ router.post('/getMeetingAttendance', function(req, res, next) {
 
     var meetingID = req.body.meetingID;
 
-    db.all("SELECT * FROM Attendance WHERE meetingID = ?", [meetingID], function (err, rows) {
+    db.all("SELECT * FROM Attendance WHERE meetingID = ? AND status = 'arrived'", [meetingID], function (err, rows) {
         if(err){
             res.json({"status": "Error finding attendance"});
         }else{
